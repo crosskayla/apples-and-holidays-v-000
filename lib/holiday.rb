b344565bd490f841387870c30dbd94926f25bfa7
Kayla Cross <kac288@georgetown.edu>
2018-03-09 02:04:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,7 +94,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       if supplies.include?("BBQ")
-        collect_symbols << BBQ
+        collect_symbols << holiday
       end
     end
   end
