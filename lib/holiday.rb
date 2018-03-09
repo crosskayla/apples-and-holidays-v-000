f3ce0c4a62677f62153a367995e28ab7706dffed
Kayla Cross <kac288@georgetown.edu>
2018-03-09 01:36:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,8 +57,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash
 end
 
-season = "fall"
-holiday_name = "columbus day"
+season = :fall
+holiday_name = :columbus_day
 supply_array = ["party stuff", "arrays"]
 
 add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
