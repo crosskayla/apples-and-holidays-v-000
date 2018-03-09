3158e98a7a6c556a0b196a28a90ab1b5d591b901
Kayla Cross <kac288@georgetown.edu>
2018-03-09 01:58:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,9 +79,9 @@ def all_supplies_in_holidays(holiday_hash)
       if holiday.to_s.include?('_')
         printable_holiday = holiday.to_s.split('_').collect{|word| word.capitalize}
         printable_holiday = printable_holiday.join(" ")
-        puts "/t #{printable_holiday}: /n #{supplies.join(", ")}"
+        puts "   #{printable_holiday}: \n #{supplies.join(", ")}"
       else
-        puts "/t #{holiday.to_s.capitalize}: /n #{supplies.join(", ")}"
+        puts "   #{holiday.to_s.capitalize}: \n #{supplies.join(", ")}"
       end
     end
   end
